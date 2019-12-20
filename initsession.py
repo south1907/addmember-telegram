21ede22d3524ae87b8e746a4c97bc17801b088f3
South <nam@google.com>
2019-12-20 23:32:50 +0700

    GET ONLY PHONE
    get data group, user only phone (fix code)

--- initsession.py
+++ initsession.py
@@ -4,9 +4,9 @@ from telethon import sync, TelegramClient, events
 
 logging.basicConfig(level=logging.WARNING)
 
-api_id = 11007498
-api_hash = '57c6f3c72c2f21676d53be2e1deab3aa'
-phone = '+84976081803'
+api_id = 1183421
+api_hash = '87fbbd1e449af4f5ce8a12734f7f8840'
+phone = '+84327959451'
 
 
 client = TelegramClient(phone, api_id, api_hash, connection=connection.ConnectionTcpMTProxyRandomizedIntermediate, proxy=('116.203.2.245', 1337, 'dd81b667f85e7e5d358de3b8e4ade6302f'))
