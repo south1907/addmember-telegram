2975af15c80ea5d41932eef8a4f845669a0713f9
Huu PhM <namph.soict@gmail.com>
2020-11-26 08:40:18 +0700

    add time active in config

--- add_member.py
+++ add_member.py
@@ -1,25 +1,22 @@
-from telethon import TelegramClient, connection
 import logging
 from telethon import sync, TelegramClient, events
-from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
 from telethon.tl.types import InputPeerChannel
 from telethon.tl.types import InputPeerUser
 from telethon.tl.functions.channels import InviteToChannelRequest
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError, FloodWaitError
-import random
 import time
 import traceback
 import datetime
 import os
 import json
 
-def get_group_by_id(groups, group_id):
 
-	for group in groups:
-		if(group_id == int(group['group_id'])):
-			return group
-	return None
+def get_group_by_id(groups, group_id):
+    for group in groups:
+        if (group_id == int(group['group_id'])):
+            return group
+    return None
+
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 print(root_path)
@@ -28,7 +25,7 @@ start_time = datetime.datetime.now()
 logging.basicConfig(level=logging.WARNING)
 
 with open('config.json', 'r', encoding='utf-8') as f:
-	config = json.loads(f.read())
+    config = json.loads(f.read())
 
 accounts = config['accounts']
 print("Total account: " + str(len(accounts)))
@@ -38,69 +35,71 @@ folder_session = 'session/'
 group_target_id = config['group_target']
 # group source
 group_source_id = config['group_source']
+#date_online_from
+from_date_active = config['from_date_active']
 
 # list client
 clients = []
 for account in accounts:
-	api_id = account['api_id']
-	api_hash = account['api_hash']
-	phone = account['phone']
+    api_id = account['api_id']
+    api_hash = account['api_hash']
+    phone = account['phone']
 
-	client = TelegramClient(folder_session + phone, api_id, api_hash)
+    client = TelegramClient(folder_session + phone, api_id, api_hash)
 
-	client.connect()
+    client.connect()
 
-	if (client.is_user_authorized()):
-		print(phone + ' login success')
-		clients.append({
-			'phone': phone,
-			'client': client
-		})
-	else:
-		print(phone + ' login fail')
+    if client.is_user_authorized():
+        print(phone + ' login success')
+        clients.append({
+            'phone': phone,
+            'client': client
+        })
+    else:
+        print(phone + ' login fail')
 
 filter_clients = []
 
 for my_client in clients:
-	phone = my_client['phone']
-	path_group = root_path + '/data/group/' + phone + '.json'
-	if os.path.isfile(path_group):
+    phone = my_client['phone']
+    path_group = root_path + '/data/group/' + phone + '.json'
+    if os.path.isfile(path_group):
 
-		with open(path_group, 'r', encoding='utf-8') as f:
-			groups = json.loads(f.read())
+        with open(path_group, 'r', encoding='utf-8') as f:
+            groups = json.loads(f.read())
 
-		current_target_group = get_group_by_id(groups, group_target_id)
+        current_target_group = get_group_by_id(groups, group_target_id)
 
-		if current_target_group:
-			group_access_hash = int(current_target_group['access_hash'])
-			target_group_entity = InputPeerChannel(group_target_id, group_access_hash)
+        if current_target_group:
+            group_access_hash = int(current_target_group['access_hash'])
+            target_group_entity = InputPeerChannel(group_target_id, group_access_hash)
 
-			path_group_user = root_path + '/data/user/' + phone + "_" + str(group_source_id) + '.json'
-			if os.path.isfile(path_group_user):
-				# add target_group_entity key value
-				my_client['target_group_entity'] = target_group_entity
+            path_group_user = root_path + '/data/user/' + phone + "_" + str(group_source_id) + '.json'
+            if os.path.isfile(path_group_user):
+                # add target_group_entity key value
+                my_client['target_group_entity'] = target_group_entity
+                with open(path_group_user, encoding='utf-8') as f:
+                    my_client['users'] = json.loads(f.read())
 
-				with open(path_group_user, encoding='utf-8') as f:
-					my_client['users'] = json.loads(f.read())
-
-				filter_clients.append(my_client)
-			else:
-				print('This account with phone ' + str(phone) + ' is not in source group')
-		else:
-			print('This account with phone ' + str(phone) + ' is not in target group')
-	else:
-		print('This account with phone do not have data. Please run get_data or init_session')
+                filter_clients.append(my_client)
+            else:
+                print('This account with phone ' + str(phone) + ' is not in source group')
+        else:
+            print('This account with phone ' + str(phone) + ' is not in target group')
+    else:
+        print('This account with phone do not have data. Please run get_data or init_session')
 
 # run
 previous_count = 0
 count_add = 0
 
 try:
-	with open(root_path + '/current_count.txt') as f:
-		previous_count = int(f.read())
+    with open(root_path + '/current_count.txt') as f:
+        previous_count = int(f.read())
 except Exception as e:
-	pass
+    pass
 
+print('From index: ' + str(previous_count))
 total_client = len(filter_clients)
 
 total_user = filter_clients[0]['users'].__len__()
@@ -108,63 +107,71 @@ total_user = filter_clients[0]['users'].__len__()
 i = 0
 while i < total_user:
 
-	# previous run
-	if(i < previous_count):
-		i += 1
-		continue
+    # previous run
+    if i < previous_count:
+        i += 1
+        continue
 
-	# count_add if added 35 user
-	if count_add % (35 * total_client) == (35 * total_client - 1):
-		print('sleep 15 minute')
-		time.sleep(15*60)
+    # count_add if added 35 user
+    if count_add % (35 * total_client) == (35 * total_client - 1):
+        print('sleep 15 minute')
+        time.sleep(15 * 60)
 
-	total_client = filter_clients.__len__()
-	print("remain client: " + str(total_client))
-	if(total_client == 0):
+    total_client = filter_clients.__len__()
+    print("remain client: " + str(total_client))
+    if total_client == 0:
+        with open(root_path + '/current_count.txt', 'w') as g:
+            g.write(str(i))
+            g.close()
 
-		with open(root_path + '/current_count.txt', 'w') as g:
-			g.write(str(i))
-			g.close()
+        print('END: accounts is empty')
+        break
 
-		print('END: accounts is empty')
-		break
+    current_index = count_add % total_client
+    print("current_index: " + str(current_index))
+    current_client = filter_clients[current_index]
+    client = current_client['client']
+    user = current_client['users'][i]
 
-	current_index = count_add % total_client
-	print("current_index: " + str(current_index))
-	current_client = filter_clients[current_index]
-	client = current_client['client']
-	user = current_client['users'][i]
-	target_group_entity = current_client['target_group_entity']
+    if user['date_online'] != 'online' and user['date_online'] < from_date_active:
+        i += 1
+        print('User ' + user['user_id'] + ' has time active: ' + user['date_online'] + ' is overdue')
+        continue
 
-	try:
-		user_to_add = InputPeerUser(int(user['user_id']), int(user['access_hash']))
-		client(InviteToChannelRequest(target_group_entity,[user_to_add]))
-		print("Add member success")
-		count_add += 1
-		print('sleep: ' + str(120/total_client))
-		time.sleep(120/total_client)
-		
-	except PeerFloodError as e:
-		print("Error Fooling cmnr")
-		traceback.print_exc()
-		print("remove client: " + current_client['phone'])
-		client.disconnect()
-		filter_clients.remove(current_client)
-		
-		# not increate i
-		continue
-	except UserPrivacyRestrictedError:
-		print("Error Privacy")
-	except:
-		print("Error other")
-		traceback.print_exc()
-		# break
+    target_group_entity = current_client['target_group_entity']
 
-	i += 1
+    try:
+        print('add member: ' + user['user_id'])
+        user_to_add = InputPeerUser(int(user['user_id']), int(user['access_hash']))
+        client(InviteToChannelRequest(target_group_entity, [user_to_add]))
+        print('Add member '+ user['user_id'] +' success')
+        count_add += 1
+        print('sleep: ' + str(120 / total_client))
+        time.sleep(120 / total_client)
 
+    except PeerFloodError as e:
+        print("Error Fooling cmnr")
+        traceback.print_exc()
+        print("remove client: " + current_client['phone'])
+        client.disconnect()
+        filter_clients.remove(current_client)
+
+        # not increate i
+        continue
+    except UserPrivacyRestrictedError:
+        print("Error Privacy")
+    except:
+        print("Error other")
+    # break
+
+    i += 1
+
+with open(root_path + '/current_count.txt', 'w') as g:
+    g.write(str(i))
+    g.close()
 print("disconnect")
 for cli in clients:
-	cli['client'].disconnect()
+    cli['client'].disconnect()
 end_time = datetime.datetime.now()
 print("total: " + str(count_add))
 print("total time: " + str(end_time - start_time))
--- get_data.py
+++ get_data.py
@@ -2,88 +2,114 @@ from telethon import TelegramClient, connection
 import logging
 from telethon import sync, TelegramClient, events
 from telethon.tl.functions.messages import GetDialogsRequest
-from telethon.tl.types import InputPeerEmpty
+from telethon.tl.types import InputPeerEmpty, UserStatusOffline, UserStatusRecently, UserStatusLastMonth, \
+    UserStatusLastWeek
 import json
+from datetime import datetime, timedelta
 
 logging.basicConfig(level=logging.WARNING)
 
+
 def get_group(phone, api_id, api_hash):
-	folder_session = 'session/'
-	client = TelegramClient(folder_session + phone, api_id, api_hash)
-	client.connect()
-	if not client.is_user_authorized():
-		print('Login fail, need to run init_session')
-	else:
-		get_data_group(client, phone)
+    folder_session = 'session/'
+    client = TelegramClient(folder_session + phone, api_id, api_hash)
+    client.connect()
+    if not client.is_user_authorized():
+        print('Login fail, need to run init_session')
+    else:
+        get_data_group(client, phone)
+
 
 def get_data_group(client, phone):
-	print('getting data ' + phone)
-	chats = []
-	last_date = None
-	chunk_size = 200
-	groups=[]
-	 
-	query = client(GetDialogsRequest(
-				 offset_date=last_date,
-				 offset_id=0,
-				 offset_peer=InputPeerEmpty(),
-				 limit=chunk_size,
-				 hash = 0
-			 ))
-	chats.extend(query.chats)
-	for chat in chats:
-		try:
-			if chat.megagroup is not None and chat.access_hash is not None:
-				groups.append(chat)
-		except:
-			continue
+    print('getting data ' + phone)
+    chats = []
+    last_date = None
+    chunk_size = 200
+    groups = []
 
-	results = []
-	for group in groups:
-		try:
-			tmp = {
-				'group_id': str(group.id),
-				'access_hash': str(group.access_hash),
-				'title': str(group.title),
-			}
-			results.append(tmp)
-			
-			if group.megagroup == True:
-				get_data_user(client, group)
-		except Exception as e:
-			print(e)
-			print('error save group')
-	with open('data/group/' + phone + '.json', 'w', encoding='utf-8') as f:
-		json.dump(results, f, indent=4, ensure_ascii=False)
+    query = client(GetDialogsRequest(
+        offset_date=last_date,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=chunk_size,
+        hash=0
+    ))
+    chats.extend(query.chats)
+    for chat in chats:
+        try:
+            if chat.megagroup is not None and chat.access_hash is not None:
+                groups.append(chat)
+        except:
+            continue
+
+    results = []
+    for group in groups:
+        try:
+            tmp = {
+                'group_id': str(group.id),
+                'access_hash': str(group.access_hash),
+                'title': str(group.title),
+            }
+            results.append(tmp)
+
+            if group.megagroup == True:
+                get_data_user(client, group)
+        except Exception as e:
+            print(e)
+            print('error save group')
+    with open('data/group/' + phone + '.json', 'w', encoding='utf-8') as f:
+        json.dump(results, f, indent=4, ensure_ascii=False)
+
 
 def get_data_user(client, group):
-	group_id = str(group.id)
-	print(group_id)
+    group_id = str(group.id)
+    print(group_id)
 
-	all_participants = []
-	all_participants = client.get_participants(group, aggressive=True)
-	results = []
-	for user in all_participants:
-		tmp = {
-			'user_id': str(user.id),
-			'access_hash': str(user.access_hash),
-			'username': str(user.username)
-		}
-		results.append(tmp)
-	with open('data/user/' + phone + "_" + group_id +'.json', 'w', encoding='utf-8') as f:
-		json.dump(results, f, indent=4, ensure_ascii=False)
+    all_participants = client.get_participants(group, aggressive=True)
+    results = []
+    today = datetime.now()
+    last_week = today + timedelta(days=-7)
+    last_month = today + timedelta(days=-30)
+    path_file = 'data/user/' + phone + "_" + group_id + '.json'
 
-		
+    for user in all_participants:
+        # print(user)
+        # print(type(user.status))
+        try:
+            if isinstance(user.status, UserStatusRecently):
+                date_online_str = 'online'
+            else:
+                if isinstance(user.status, UserStatusLastMonth):
+                    date_online = last_month
+                if isinstance(user.status, UserStatusLastWeek):
+                    date_online = last_week
+                if isinstance(user.status, UserStatusOffline):
+                    date_online = user.status.was_online
+
+                date_online_str = date_online.strftime("%Y%m%d")
+            tmp = {
+                'user_id': str(user.id),
+                'access_hash': str(user.access_hash),
+                'username': str(user.username),
+                "date_online": date_online_str
+            }
+            results.append(tmp)
+        except:
+            print("Error get user")
+    with open(path_file, 'w', encoding='utf-8') as f:
+        json.dump(results, f, indent=4, ensure_ascii=False)
+
+
 with open('config.json', 'r', encoding='utf-8') as f:
-	config = json.loads(f.read())
+    config = json.loads(f.read())
 
 accounts = config['accounts']
 
 folder_session = 'session/'
 
 for account in accounts:
-	api_id = account['api_id']
-	api_hash = account['api_hash']
-	phone = account['phone']
-	print(phone)
-	get_group(phone, api_id, api_hash)
+    api_id = account['api_id']
+    api_hash = account['api_hash']
+    phone = account['phone']
+    print(phone)
+    get_group(phone, api_id, api_hash)
